41aaea7710f8f99c2202b0a9772f874ae8acb86d
INDIG0N <hicksjordan1999@gmail.com>
2021-10-30 15:05:37 -0400

    updated eda and seperated function definitions

--- src/NYC_air_quality.R
+++ src/NYC_air_quality.R
@@ -1,21 +1,40 @@
 library(ggplot2)
 library(dplyr)
 library(shiny)
+library(tidyr)
 
-df = read.csv('Air_Quality.csv')
+# FUNCTIONS
+##########
+piv_wide_on_name = function(your.frame) {
+  df = pivot_wider(your.frame, names_from = Name, values_from = Data.Value)
+  return(df)
+}
+##########
 
-head(df)
 
-summary(df)
+df = read.csv('../data/Air_Quality.csv')
 
-trimmed = df[, c(-7, -12)]
+df['Time.Period'] = as.character(df$Time.Period)
+df = filter(df, grepl('Annual Average', df$Time.Period))
+
+trimmed = df[, c(-2, -7, -12)]
 trimmed = trimmed[trimmed$Measure != 'million miles',]
 trimmed = trimmed[trimmed$Name != 'Boiler Emissions- Total SO2 Emissions',]
 trimmed = trimmed[trimmed$Name != 'Boiler Emissions- Total NOx Emissions',]
 trimmed = trimmed[trimmed$Name != 'Boiler Emissions- Total PM2.5 Emissions',]
 
-head(trimmed)
+particle_rows = unique(trimmed$Name)[c(1, 5, 8, 9, 11)]
+O3_rows = unique(trimmed$Name)[c(2, 10, 12, 13)]
+SO2_rows = unique(trimmed$Name)[3]
+NO2_rows = unique(trimmed$Name)[4]
+Benzene_rows = unique(trimmed$Name)[6]
+Formaldehyde_rows = unique(trimmed$Name)[7]
 
-summary(trimmed)
+particle_df = filter(trimmed, Name %in% particle_rows)
+O3_df = filter(trimmed, Name %in% O3_rows)
+SO2_df = filter(trimmed, Name == SO2_rows)
+NO2_df = filter(trimmed, Name == NO2_rows)
+Benzene_df = filter(trimmed, Name == Benzene_rows)
+Formaldehyde_df = filter(trimmed, Name == Formaldehyde_rows)
 
-unique(trimmed$Name)
+summary(particle_df)
